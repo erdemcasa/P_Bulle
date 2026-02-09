c69ffc06de77d9c753957b54cad877f155db639d
erdemcasa <okkes.kose@eduvaud.ch>
2026-02-09 21:02:53 +0100

    Sauvegarde.

--- P_Flashcards/app/controllers/decks_controller.ts
+++ P_Flashcards/app/controllers/decks_controller.ts
@@ -6,9 +6,16 @@ export default class DecksController {
    * Display a list of resource
    */
   async index({ view }: HttpContext) {
-
     const decks = await Deck.query().orderBy('title', 'asc')
 
     return view.render('pages/home', { decks })
-    
-  }}
+  }
+  /**
+   * Show individual record
+   */
+  async show({ params, view }: HttpContext) {
+    const deck = await Deck.query().where('id', params.id).preload('deck').firstOrFail()
+
+    return view.render('pages/teachers/show.edge', { title: "Détail d'unenseignant", teacher })
+  }
+}
--- P_Flashcards/start/routes.ts
+++ P_Flashcards/start/routes.ts
@@ -11,3 +11,4 @@ const DecksController = () => import('#controllers/decks_controller')
 import router from '@adonisjs/core/services/router'
 
 router.get('/', [DecksController, 'index']).as('home')
+router.get('/decks/:id', [DecksController, 'index']).as('decks')
